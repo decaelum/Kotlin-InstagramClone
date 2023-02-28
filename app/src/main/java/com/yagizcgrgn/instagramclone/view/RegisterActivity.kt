6a3ce33c7c098e0b4b746412446b1617ea5ebed9
decaelum <yagiz.cgrng@gmail.com>
2023-03-01 00:44:33 +0300

    Break time (save commit)

--- app/src/main/java/com/yagizcgrgn/instagramclone/view/RegisterActivity.kt
+++ app/src/main/java/com/yagizcgrgn/instagramclone/view/RegisterActivity.kt
@@ -1,24 +1,65 @@
 package com.yagizcgrgn.instagramclone.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.yagizcgrgn.instagramclone.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
+        auth = FirebaseAuth.getInstance()
         setContentView(view)
     }
 
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+    }
+
     fun registerSave(view: View){
+        val intent = Intent(this@RegisterActivity,MainActivity::class.java)
+        val email = binding.edtEmail.toString()
+        val password = binding.edtPassword.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            if (email.isEmpty() || password.isEmpty()){
+                binding.btRegisterSave.isEnabled = false
+            }
+            try {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Log.d("NEWUSER", "New user added")
+                            val user = auth.currentUser
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Log.d("NEWUSER", "New user adding failed", task.exception)
+                            Toast.makeText(this@RegisterActivity, "Adding Failed", Toast.LENGTH_LONG
+                            ).show()
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+            }catch (e : Exception){
+                print(e.stackTrace)
+            }
+        }
+
 
     }
 
     fun cancel(view: View){
 
     }
+
 }
--- app/src/main/java/com/yagizcgrgn/instagramclone/view/UploadActivity.kt
+++ app/src/main/java/com/yagizcgrgn/instagramclone/view/UploadActivity.kt
@@ -1,11 +1,22 @@
 package com.yagizcgrgn.instagramclone.view
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.yagizcgrgn.instagramclone.databinding.ActivityUploadBinding
 
 class UploadActivity : AppCompatActivity() {
     lateinit var binding: ActivityUploadBinding
+    lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    lateinit var permissionLauncher: ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
@@ -13,11 +24,71 @@ class UploadActivity : AppCompatActivity() {
         setContentView(view)
     }
 
-    fun share(){
+    fun share(view: View) {
 
     }
 
-    fun selectImage(){
+    fun selectImage(view: View) {
+        //Android 33+ -> READ_MEDIA_IMAGES
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        android.Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    //rationale
+                    Snackbar.make(view, "Permission Need!!!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission", View.OnClickListener {
+                            //request permission
+                            permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                } else {
+                    //request permission
+                    permissionLauncher.launch(android.Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            } else {
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                //intent
+                activityResultLauncher.launch(intentToGallery)
+            }
+        } else {
+            //Android 32- -> READ_EXTERNAL_STORAGE
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    //rationale
+                    Snackbar.make(view, "Permission Need!!!", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission", View.OnClickListener {
+                            //request permission
+                            permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }).show()
+                } else {
+                    //request permission
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            } else {
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                //intent
+                activityResultLauncher.launch(intentToGallery)
+            }
+
+
+        }
+
 
     }
 }
