338451092b31de07a1dc7384362e2f529ec638da
decaelum <yagiz.cgrng@gmail.com>
2023-03-01 12:49:20 +0300

    Login and register functions added

--- app/src/main/java/com/yagizcgrgn/instagramclone/view/MainActivity.kt
+++ app/src/main/java/com/yagizcgrgn/instagramclone/view/MainActivity.kt
@@ -1,26 +1,62 @@
 package com.yagizcgrgn.instagramclone.view
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 import com.yagizcgrgn.instagramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth : FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        auth = Firebase.auth
+
+        val currentUser = auth.currentUser
+        TODO("Reminder button will add")
         setContentView(view)
 
     }
-
     fun login(view: View) {
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
+        if (email.isEmpty() || password.isEmpty()){
+            binding.btLogin.isEnabled = false
+        }
+        try {
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this){task ->
+                    if (task.isSuccessful){
+                        Log.d("LOGIN", "Login successful")
+                        val intent = Intent(this@MainActivity,FeedActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Log.d("LOGIN","Login Failed")
+                        Toast.makeText(this@MainActivity,"Try Again",Toast.LENGTH_LONG).show()
+                        binding.edtPassword.setText("")
+                    }
+                }
+        }catch (e : Exception){
+            print(e.stackTrace)
+        }
 
     }
 
     fun register(view: View){
-
+        val intent = Intent(this@MainActivity,RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/yagizcgrgn/instagramclone/view/RegisterActivity.kt
+++ app/src/main/java/com/yagizcgrgn/instagramclone/view/RegisterActivity.kt
@@ -13,11 +13,18 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var auth : FirebaseAuth
 
+    private var intent : Intent? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
-        val view = binding.root
         auth = FirebaseAuth.getInstance()
+
+        intent  =  Intent(this@RegisterActivity,MainActivity::class.java)
+        val view = binding.root
+
+
         setContentView(view)
     }
 
@@ -27,39 +34,38 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     fun registerSave(view: View){
-        val intent = Intent(this@RegisterActivity,MainActivity::class.java)
-        val email = binding.edtEmail.toString()
-        val password = binding.edtPassword.toString()
-        if (email.isNotEmpty() && password.isNotEmpty()){
-            if (email.isEmpty() || password.isEmpty()){
-                binding.btRegisterSave.isEnabled = false
-            }
-            try {
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            Log.d("NEWUSER", "New user added")
-                            val user = auth.currentUser
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            Log.d("NEWUSER", "New user adding failed", task.exception)
-                            Toast.makeText(this@RegisterActivity, "Adding Failed", Toast.LENGTH_LONG
-                            ).show()
-                            startActivity(intent)
-                            finish()
-                        }
-                    }
-            }catch (e : Exception){
-                print(e.stackTrace)
-            }
-        }
+        val email = binding.edtEmail.text.toString()
+        val password = binding.edtPassword.text.toString()
 
+        if (email.isEmpty() || password.isEmpty()){
+            binding.btRegisterSave.isEnabled = false
+        }
+        try {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this@RegisterActivity) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("NEWUSER", "New user added")
+                        val user = auth.currentUser
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Log.d("NEWUSER", "New user adding failed", task.exception)
+                        Toast.makeText(this@RegisterActivity, "Adding Failed", Toast.LENGTH_LONG
+                        ).show()
+                        startActivity(intent)
+                        finish()
+                        TODO("Snackbar add(optional)")
+                    }
+                }
+        }catch (e : Exception){
+            print(e.stackTrace)
+        }
 
     }
 
     fun cancel(view: View){
-
+        startActivity(intent)
+        finish()
     }
 
 }
